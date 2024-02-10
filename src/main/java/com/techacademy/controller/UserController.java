ace7c79634cd970a94e1c6471c8a57a782b1311a
toki-0625 <tokiaya0625@gmail.com>
2024-02-10 19:12:27 +0900

    Lesson18課題

---
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -0,0 +1,98 @@
+package com.techacademy.controller;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult; // 追加
+import org.springframework.validation.annotation.Validated; // 追加
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.techacademy.entity.User;
+import com.techacademy.service.UserService;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    // 一覧画面を表示
+    @GetMapping("/list")
+    public String getList(Model model) {
+        // 全件検索結果をModelに登録
+        model.addAttribute("userlist", service.getUserList());
+        // user/list.htmlに画面遷移
+        return "user/list";
+    }
+
+    // User登録画面を表示
+    @GetMapping("/register")
+    public String getRegister(@ModelAttribute User user) {
+        // User登録画面に遷移
+        return "user/register";
+    }
+
+    // User登録処理
+    @PostMapping("/register")
+    public String postRegister(@Validated User user, BindingResult res, Model model) {
+        if (res.hasErrors()) {
+            // エラーあり
+            return getRegister(user);
+        }
+        // User登録
+        service.saveUser(user);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
+
+    // User更新画面を表示
+    @GetMapping("/update/{id}/")
+    public String getUser(@PathVariable("id") Integer id, Model model, User user) {
+
+        // idがnullかどうか判断
+        if (id != null) {
+            // idがnullではない場合、一覧画面から遷移し、Modelに登録する
+            model.addAttribute("user", service.getUser(id));
+        } else {
+            // idがnullの場合、postUser()から遷移し、ModelにはpostUser()から渡された引数のuserをセットする
+            model.addAttribute("user", user);
+        }
+        // User更新画面に遷移
+        return "user/update";
+    }
+
+    // User更新処理
+    @PostMapping("/update/{id}/")
+    public String postUser(@PathVariable("id") Integer id, @Validated User user, BindingResult res, Model model) {
+
+        // エラーがあるかどうか判断
+        if (res.hasErrors()) {
+            // エラーありの場合、getUserメソッドを呼び出す。引数にnullを設定する
+            return getUser(null, model, user);
+        }
+        // エラー無しの場合
+
+        // User登録
+        service.saveUser(user);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
+
+    // User削除処理
+    @PostMapping(path = "list", params = "deleteRun")
+    public String deleteRun(@RequestParam(name = "idck") Set<Integer> idck, Model model) {
+        // Userを一括削除
+        service.deleteUser(idck);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
+}
---
+++ src/main/java/com/techacademy/service/UserService.java
@@ -0,0 +1,46 @@
+package com.techacademy.service;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+import com.techacademy.entity.User;
+import com.techacademy.repository.UserRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository repository) {
+        this.userRepository = repository;
+    }
+
+    //全件を検索して返す
+    public List<User> getUserList() {
+        // リポジトリのfindAllメソッドを呼び出す
+        return userRepository.findAll();
+    }
+    
+ // 追記
+    //Userを1件検索して返す
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+    
+    //追記
+    //Userの登録を行う
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+    
+    //追記
+    //Userの削除を行う
+    @Transactional
+    public void deleteUser(Set<Integer> idck) {
+        for(Integer id : idck) {
+            userRepository.deleteById(id);
+        }
+    }
+}
